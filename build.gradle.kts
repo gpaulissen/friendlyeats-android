566f74d9b3c89be675b622125ea439e40cc1dd85
Gert-Jan Paulissen <12508981+gpaulissen@users.noreply.github.com>
2025-04-02 19:35:18 +0200

    Using libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,23 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
+    /*
     id("com.android.application") version "8.9.1" apply false
     id("com.android.library") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.20" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("androidx.navigation.safeargs") version "2.8.9" apply false
     id("com.github.ben-manes.versions") version "0.52.0" apply true
-    id("com.google.firebase.appdistribution") version "5.1.1" apply false
+    id("com.google.firebase.appdistribution") version "5.1.1" apply false   
+    */
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.jetbrains.kotlin) apply false
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.androidx.navigation.safeargs) apply false
+    alias(libs.plugins.github.ben.manes.versions) apply false
+    alias(libs.plugins.google.firebase.appdistribution) apply false
+
 }
 
 allprojects {
