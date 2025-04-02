81d7b3a0da996bfe1a4d9f671770369e5f5d03fe
Gert-Jan Paulissen <12508981+gpaulissen@users.noreply.github.com>
2025-04-02 17:39:30 +0200

    step 7

--- app/src/main/java/com/google/firebase/example/fireeats/MainFragment.kt
+++ app/src/main/java/com/google/firebase/example/fireeats/MainFragment.kt
@@ -25,6 +25,7 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.example.fireeats.databinding.FragmentMainBinding
 import com.google.firebase.example.fireeats.adapter.RestaurantAdapter
 import com.google.firebase.example.fireeats.model.Restaurant
+import com.google.firebase.example.fireeats.util.RestaurantUtil
 import com.google.firebase.example.fireeats.viewmodel.MainActivityViewModel
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
@@ -70,6 +71,11 @@ class MainFragment : Fragment(),
 
         // Firestore
         firestore = Firebase.firestore
+
+        // Get the 50 highest rated restaurants
+        query = firestore.collection("restaurants")
+            .orderBy("avgRating", Query.Direction.DESCENDING)
+            .limit(LIMIT.toLong())
 
         // RecyclerView
         query?.let {
@@ -207,8 +213,14 @@ class MainFragment : Fragment(),
     }
 
     private fun onAddItemsClicked() {
-        // TODO(developer): Add random restaurants
-        showTodoToast()
+        val restaurantsRef = firestore.collection("restaurants")
+        for (i in 0..9) {
+            // Create random restaurant / ratings
+            val randomRestaurant = RestaurantUtil.getRandom(requireContext())
+
+            // Add restaurant
+            restaurantsRef.add(randomRestaurant)
+        }
     }
 
     private fun showSignInErrorDialog(@StringRes message: Int) {
